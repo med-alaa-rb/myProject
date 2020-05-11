d1877b1a36cd4e0664f309b734b00d6e8026d824
med-alaa-rb <alaa.rabai@gmail.com>
2020-05-11 18:37:30 +0100

    a

---
+++ peb.js
@@ -0,0 +1,4 @@
+$('body').css('background-image', 'url(backgroundProject.jpg)').css('background-size', 'contain')
+$('h1').css('border','2px dotted DarkKhaki')
+$('img').css('opacity','0.8').css('border','2px solid DarkGoldenRod')
+$('img').css('margin-left','180px').css('width','900px').css('height','300px')
